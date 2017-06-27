d17d11d9bc287d2065e62723e4cc37cdbc8cd5bd
doulighan <doulighan@gmail.com>
2017-06-27 11:14:06 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default class EyesOnMe extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  focused () {
+    console.log('Good!')
+  }
+
+  blurred () {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render () {
+    return (
+      <div>
+        <button onFocus={this.focused} onBlur={this.blurred} />
+      </div>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Keypad extends React.Component {
+  log () {
+    console.log('Entering password...')
+  }
+
+  render () {
+    return (
+      <div>
+        <input onKeyUp={this.log} type='password' />
+      </div>
+    )
+  }
+}
